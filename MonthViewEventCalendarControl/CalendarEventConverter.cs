df3bdf6e284ae3245e0b50b3cb2cc5c45861da7e
Ha Phi Hung <haphihung55@gmail.com>
2021-11-24 16:35:38 +0700

    Add custom month view calendar with event support and reorganize styles

---
+++ MonthViewEventCalendarControl/CalendarEventConverter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Windows.Data;
+
+namespace TienIchLich.MonthViewEventCalendarControl
+{
+    /// <summary>
+    /// Get events for the current CalendarDayButton.
+    /// </summary>
+    [ValueConversion(typeof(ObservableCollection<CalendarEventViewModel>), typeof(ObservableCollection<CalendarEventViewModel>))]
+    public class CalendarEventConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            DateTime date = (DateTime)values[1];
+
+            var thisDayEvents = new ObservableCollection<CalendarEventViewModel>();
+            foreach (CalendarEventViewModel calendarEvent in (ObservableCollection<CalendarEventViewModel>)values[0])
+            {
+                var startTime = calendarEvent.StartTime;
+                var startDate = new DateTime(startTime.Year, startTime.Month, startTime.Day);
+                if (startDate == date)
+                    thisDayEvents.Add(calendarEvent);
+            }
+
+            return thisDayEvents;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ MonthViewEventCalendarControl/MonthViewEventCalendar.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace TienIchLich.MonthViewEventCalendarControl
+{
+    public class MonthViewEventCalendar : Calendar
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public static DependencyProperty CalendarEventsProperty =
+            DependencyProperty.Register
+            (
+                "CalendarEvents",
+                typeof(ObservableCollection<CalendarEventViewModel>),
+                typeof(Calendar)
+            );
+
+        public ObservableCollection<CalendarEventViewModel> CalendarEvents
+        {
+            get { return (ObservableCollection<CalendarEventViewModel>)GetValue(CalendarEventsProperty); }
+            set { SetValue(CalendarEventsProperty, value); }
+        }
+
+        static MonthViewEventCalendar()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(MonthViewEventCalendar), 
+                                                     new FrameworkPropertyMetadata(typeof(MonthViewEventCalendar)));
+        }
+
+        public MonthViewEventCalendar()
+            : base()
+        {
+            SetValue(CalendarEventsProperty, new ObservableCollection<CalendarEventViewModel>());
+        }
+
+        //protected override void OnMouseDoubleClick(MouseButtonEventArgs e)
+        //{
+        //    base.OnMouseDoubleClick(e);
+
+        //    FrameworkElement element = e.OriginalSource as FrameworkElement;
+
+        //    if (element.DataContext is DateTime)
+        //    {
+        //        EventEditor eventEditor = new EventEditor
+        //        (
+        //            (CalendarEventViewModel calendarEvent) =>
+        //            {
+        //                Appointments.Add(calendarEvent);
+        //                if (PropertyChanged != null)
+        //                {
+        //                    PropertyChanged(this, new PropertyChangedEventArgs("Appointments"));
+        //                }
+        //            }
+        //        );
+        //        appointmentWindow.Show();
+        //    }
+        //}
+    }
+}
