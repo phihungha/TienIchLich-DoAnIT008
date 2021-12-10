61ab906f034d7627e101a30de78a41fa77572b34
Ha Phi Hung <haphihung55@gmail.com>
2021-12-10 07:44:00 +0700

    Implement upcoming overview start time filtering

---
+++ ViewModels/Converters/StartTimeFilterOptionToString.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace TienIchLich.ViewModels
+{
+    public class StartTimeFilterOptionToString : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            UpcomingOverviewVM.StartTimeFilterOptionId optionId;
+            if (value != null && value is UpcomingOverviewVM.StartTimeFilterOptionId)
+                optionId = (UpcomingOverviewVM.StartTimeFilterOptionId)value;
+            else
+                return "";
+
+            switch (optionId)
+            {
+                case UpcomingOverviewVM.StartTimeFilterOptionId.Week1:
+                    return "1 tuần";
+                case UpcomingOverviewVM.StartTimeFilterOptionId.Week2:
+                    return "2 tuần";
+                case UpcomingOverviewVM.StartTimeFilterOptionId.Month1:
+                    return "1 tháng";
+                case UpcomingOverviewVM.StartTimeFilterOptionId.Month6:
+                    return "6 tháng";
+                case UpcomingOverviewVM.StartTimeFilterOptionId.Year1:
+                    return "1 năm";
+                case UpcomingOverviewVM.StartTimeFilterOptionId.All:
+                    return "Tất cả";
+                case UpcomingOverviewVM.StartTimeFilterOptionId.Custom:
+                    return "Tùy chọn";
+                default:
+                    return optionId.ToString();
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- ViewModels/UpcomingOverviewVM.cs
+++ ViewModels/UpcomingOverviewVM.cs
@@ -23,9 +23,87 @@ namespace TienIchLich.ViewModels
             }
         }
 
+        public enum StartTimeFilterOptionId
+        {
+            Week1,
+            Week2,
+            Month1,
+            Month6,
+            Year1,
+            All,
+            Custom
+        }
+
+        public struct StartTimeFilterOption
+        {
+            public StartTimeFilterOptionId Id { get; set; }
+            public TimeSpan Timespan { get; set; }
+        }
+
         private CollectionViewSource eventCollectionViewSource;
+        private static StartTimeFilterOption[] startTimeFilterOptions =
+        {
+            new StartTimeFilterOption() { Id = StartTimeFilterOptionId.Week1, Timespan = new TimeSpan(7, 0, 0, 0) },
+            new StartTimeFilterOption() { Id = StartTimeFilterOptionId.Week2, Timespan = new TimeSpan(14, 0, 0, 0) },
+            new StartTimeFilterOption() { Id = StartTimeFilterOptionId.Month1, Timespan = new TimeSpan(31, 0, 0, 0) },
+            new StartTimeFilterOption() { Id = StartTimeFilterOptionId.Month6, Timespan = new TimeSpan(186, 0, 0, 0) },
+            new StartTimeFilterOption() { Id = StartTimeFilterOptionId.Year1, Timespan = new TimeSpan(366, 0, 0, 0) },
+            new StartTimeFilterOption() { Id = StartTimeFilterOptionId.All },
+            new StartTimeFilterOption() { Id = StartTimeFilterOptionId.Custom, Timespan = new TimeSpan(1, 0, 0, 0) }
+        };
+        private bool useCustomStartTimeFilter;
+        private StartTimeFilterOption selectedStartTimeFilterOption;
+        private TimeSpan startTimeFilterValue;
+
 
         public ICollectionView EventCollectionView => eventCollectionViewSource.View;
+
+        public StartTimeFilterOption[] StartTimeFilterOptions => startTimeFilterOptions;
+
+        public bool UseCustomStartTimeFilter
+        {
+            get
+            {
+                return useCustomStartTimeFilter;
+            }
+            set
+            {
+                useCustomStartTimeFilter = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        public TimeSpan StartTimeFilterValue
+        {
+            get
+            {
+                return startTimeFilterValue;
+            }
+            set
+            {
+                startTimeFilterValue = value;
+                this.EventCollectionView.Refresh();
+                NotifyPropertyChanged();
+            }
+        }
+
+        public StartTimeFilterOption SelectedStartTimeFilterOption
+        {
+            get
+            {
+                return selectedStartTimeFilterOption;
+            }
+            set
+            {
+                selectedStartTimeFilterOption = value;
+                if (value.Id == StartTimeFilterOptionId.Custom)
+                    UseCustomStartTimeFilter = true;
+                else
+                    UseCustomStartTimeFilter = false;
+                StartTimeFilterValue = value.Timespan;
+                NotifyPropertyChanged();
+            }
+        }
 
         public UpcomingOverviewVM(ObservableCollection<CalendarEventVM> eventVMs)
         {
@@ -43,12 +121,17 @@ namespace TienIchLich.ViewModels
             this.eventCollectionViewSource.LiveFilteringProperties.Add("CalendarCategoryVM.IsDisplayed");
             this.eventCollectionViewSource.LiveSortingProperties.Add("StartTime");
             this.eventCollectionViewSource.LiveGroupingProperties.Add("StartTime");
+
+            this.SelectedStartTimeFilterOption = this.StartTimeFilterOptions[0];
         }
 
         private void EventCollectionViewSource_Filter(object sender, FilterEventArgs e)
         {
             var eventVM = (CalendarEventVM)e.Item;
-            e.Accepted = eventVM.CalendarCategoryVM.IsDisplayed;
+            bool startTimeAccepted = this.SelectedStartTimeFilterOption.Id == StartTimeFilterOptionId.All ||
+                                     (eventVM.StartTime < (DateTime.Now + this.StartTimeFilterValue) 
+                                      && eventVM.StartTime > DateTime.Now);
+            e.Accepted = eventVM.CalendarCategoryVM.IsDisplayed && startTimeAccepted;
         }
 
 
